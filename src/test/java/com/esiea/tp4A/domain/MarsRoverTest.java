335ab11103b5d4aa5a59c9639e6fcb0ee421a62b
matthieu <matthieu.91250@gmail.com>
2020-04-12 20:55:35 +0200

    modif

--- src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 
-public class AvancerMarsRoverTest {
+public class MarsRoverTest {
     PlanetMapImpl mapTest= new PlanetMapImpl(100);
 
 
