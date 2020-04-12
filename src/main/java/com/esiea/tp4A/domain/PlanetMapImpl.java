3cef7ad6a6d3da7a560e31742126fbfd43f62b2b
Anubis091 <46956434+Anubis091@users.noreply.github.com>
2020-04-12 20:11:46 +0200

    Update PlanetMapImpl.java

--- src/main/java/com/esiea/tp4A/domain/PlanetMapImpl.java
+++ src/main/java/com/esiea/tp4A/domain/PlanetMapImpl.java
@@ -7,19 +7,23 @@ import static java.lang.StrictMath.random;
 import static java.lang.StrictMath.round;
 
 public class PlanetMapImpl implements PlanetMap {
-    int frontN = 50;
-    int frontS = -49;
-    int frontE = 50;
-    int frontW = -49;
-    int taille = 100;
+     private int taille;
+
+    public PlanetMapImpl(int i) {
+        taille = i;
+    }
+
+    public  int getTaille() {
+        return taille;
+    }
 
     @Override
     public Set<Position> obstaclePositions() {
         Set<Position> hashSet = new HashSet<>();
-        for (int i = 0; i<round(0.15*100*100); i++) {
-            int x = (int) (random()*taille)-49;
-            int y = (int) (random()*taille)-49;
-            if(hashSet.contains(Position.of(x,y,null)) == false) {
+        for (int i = 0; i<round(0.15*taille*taille); i++) {
+            int x = (int) (random()*taille)-taille/2+1;
+            int y = (int) (random()*taille)-taille/2+1;
+            if(!hashSet.contains(Position.of(x, y, null))) {
                 hashSet.add(Position.of(x,y,null));
             }
         }
